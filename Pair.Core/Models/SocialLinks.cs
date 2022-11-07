c3a02c2d2b3aefaa64f7018c82d7777bfed28bad
NKT <49834917+ADRNV@users.noreply.github.com>
2022-11-07 17:05:16 +0300

    Create domain models

---
+++ Pair.Core/Models/SocialLinks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pair.Core.Models
+{
+    public class SocialLinks
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Link { get; set; } = string.Empty;
+    }
+}
---
+++ Pair.Core/Repositories/IPersonsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Pair.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pair.Core.Repositories
+{
+    public interface IPersonsRepository
+    {
+        Task<int> Create(Person person);
+
+        Task<int> Update(Person person, int id);
+
+        Task<bool> Remove(int id);
+
+        Task<IEnumerable<Person>> Get();
+
+        Task<Person> Get(int id);
+    }
+}
