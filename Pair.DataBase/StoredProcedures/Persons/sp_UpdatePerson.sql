4b6aab894958a103235a7ac742a02041548b3af5
Lamer0 <combinerk@gmail.com>
2022-12-04 00:24:00 +0300

    Fix update procedure for new db sheme

--- Pair.DataBase/StoredProcedures/Persons/sp_UpdatePerson.sql
+++ Pair.DataBase/StoredProcedures/Persons/sp_UpdatePerson.sql
@@ -1,8 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[sp_UpdatePerson]
 	@Id int = 0,
 	@Name VARCHAR(64),
+	@Bio VARCHAR(MAX),
 	@Age TINYINT,
 	@Sex VARCHAR(10)
 AS
-	UPDATE Persons SET Name = @Name, Age = @Age, Sex = @Sex WHERE Id = @id
+	UPDATE Persons SET Name = @Name, Bio = @Bio, Age = @Age, Sex = @Sex WHERE Id = @id
 RETURN 0
