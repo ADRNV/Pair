5aaf4bae95f2b84b2e5c803438a482c385205508
NKT <49834917+ADRNV@users.noreply.github.com>
2022-12-19 14:16:46 +0300

    Add pagination

--- Pair.Infrastructure/DapperORM/DapperRepositoryBase.cs
+++ Pair.Infrastructure/DapperORM/DapperRepositoryBase.cs
@@ -58,9 +58,13 @@ namespace Pair.Infrastructure.DapperORM
         public async virtual Task<IEnumerable<TEntity>> Get() =>
             await _connection.GetAllAsync<TEntity>();
 
+        public async virtual Task<IEnumerable<TEntity>> Get(int pageNumber, int pageSize) =>
+            await _connection.GetPagedAsync<TEntity>(pageNumber, pageSize);
+       
         public async virtual Task<TEntity> Get(int id) =>
             await _connection.GetAsync<TEntity>(id);
 
+       
         public async virtual Task<bool> Delete(TEntity obj) =>
             await _connection.DeleteAsync(obj);
 
